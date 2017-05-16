9d1747ab706ad6769656c2d3154ef83b623417a8
Anderson Méndez <anderson.mendez@gft.com>
2017-05-16 11:42:38 +0200

    fix challenge 2

--- Challenge 2 - Bowling/exercise2.js
+++ Challenge 2 - Bowling/exercise2.js
@@ -3,25 +3,50 @@ const fs = require('fs');
 init();
 
 function init() {
-    let data = readFile('testInput.txt').split('\n');
+    let data = readFile('submitInput.txt').split('\n');
     let numCases = data.shift();
     let cases = [];
     let i = 0;
     while(numCases) {
-        cases[cases.length + 1] = getCases(data[i], data[i + 1].split(' '));
+        cases[cases.length] = getCases(data[i], data[i + 1].split(' '));
         i+=2;
         numCases--;
-    }
+    }    
+    writeFile('submitOutput.txt',formatContent(cases).join('\n'));
 }
 
 function getCases(numRolls, pins) {
-    let cases = [];
-    let acum = 0;    
-    for(let i = 0; i < pins.length; i++) {
-        acum = acum + parseInt(pins[i], 10) + parseInt(pins[i + 1], 10);
-        cases.push(acum);
-    }
-    console.log(cases);
+    let points = [];
+    let acum = 0;
+    let frames = 20;
+    for(let i = 0; i < frames; i+=2) {        
+        let roll1 = parseInt(pins[i]);
+        let roll2 = parseInt(pins[i + 1]);
+        let roll3 = parseInt(pins[i + 2]);
+
+        if(roll1 === 10) { // Strike
+            acum += roll1 + roll2 + roll3;
+            points.push(acum)
+            i--;
+            frames--;
+            continue;
+        }
+
+        if(roll1 + roll2 === 10) { // Spare            
+            acum += roll1 + roll2 + roll3;
+            points.push(acum)
+            continue;            
+        }
+
+        // Normal
+        acum = acum + parseInt(roll1) + parseInt(roll2);
+        points.push(acum);
+    }    
+    return points;
+}
+
+function formatContent(cases) {
+    return cases.map((e, i) => `Case #${i + 1}: ${e.join(' ')}`);
 }
 
 function writeFile(file, content) {
